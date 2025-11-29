a02499ee31cc72ba585aec7e5fa1101ac8cdd606
Omar <omar.ssenhaji@gmail.com>
2025-11-29 20:02:12 +0100

    Add javascript selectors

---
+++ form.js
@@ -0,0 +1,18 @@
+/* -------------------------------------------------- SELECTORS ------------------------------------------------------------ */
+const form = document.querySelector(".form");
+const email = document.getElementById("email");
+const country = document.getElementById("country");
+const postal = document.getElementById("postal");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirm-password");
+
+const emailError = document.querySelector(".email-error");
+const countryError = document.querySelector(".country-error");
+const postalError = document.querySelector(".postal-error");
+const passwordError = document.querySelector(".password-error");
+const confirmPasswordError = document.querySelector(".confirm-password-error");
+/* -------------------------------------------------- EVENT LISTENERS ------------------------------------------------------------ */
+
+/* -------------------------------------------------- FORM SUBMISSION ------------------------------------------------------------ */
+
+/* -------------------------------------------------- INPUT VALIDITY CHECK ------------------------------------------------------------ */
